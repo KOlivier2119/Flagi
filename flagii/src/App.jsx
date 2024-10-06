8f0739bc5082bb157aeda0ede00b45941900d5ec
KWIZERA Olivier <kwizeraolivier2119@gmail.com>
2024-10-06 09:35:19 +0200

    Making a navbar.

--- flagii/src/App.jsx
+++ flagii/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Landing from "./components/Landing"
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <div className="min-w-screen overflow-hidden">
+      <Navbar />
       <Landing />
     </div>
   )
---
+++ flagii/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-screen bg-blue-500 flex justify-center md:justify-between p-3'>
+      <h1 className='text-white text-3xl font-bold'>Flagii</h1>
+    </div>
+  )
+}
+
+export default Navbar
